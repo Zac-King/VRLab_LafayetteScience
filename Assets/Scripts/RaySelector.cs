f5ff06850f11fc90c2cbd34471e28debee51d101
Zac King <ZacKingx@gmail.com>
2017-05-24 19:04:43 -0500

    Vive assets

---
+++ Assets/Scripts/RaySelector.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class RaySelector : MonoBehaviour
+{
+    [SerializeField] LineRenderer m_line;
+    [SerializeField] LayerMask m_mask;
+    [SerializeField] private float m_range = 500;
+    [SerializeField] private Hand m_hand;
+
+    private RaycastHit m_rayCursor;
+    private GameObject m_selectedGameobject;
+
+    private void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out m_rayCursor, m_range, m_mask))
+        {
+            m_line.SetPosition(0, gameObject.transform.position);
+            m_line.SetPosition(1, m_rayCursor.point);
+
+
+            GameObject g = m_rayCursor.collider.gameObject;
+
+            if (g.GetComponent<MenuItemSelectable>() != null)
+            {
+                g.GetComponent<MenuItemSelectable>().RayOn();
+                m_line.SetColors(Color.blue, Color.blue);
+
+                if(m_selectedGameobject != g)
+                {
+                    if (m_selectedGameobject != null)
+                        m_selectedGameobject.GetComponent<MenuItemSelectable>().RayOff();
+                    m_selectedGameobject = g;
+                }
+            }
+            else
+            {
+                if(m_selectedGameobject != null)
+                    m_selectedGameobject.GetComponent<MenuItemSelectable>().RayOff();
+
+                m_line.SetColors(Color.green, Color.green);
+                m_selectedGameobject = null;
+            }
+
+        }
+
+        if(m_hand.controller.GetHairTriggerDown() && m_selectedGameobject != null)
+        {
+            m_selectedGameobject.GetComponent<MenuItemSelectable>().ItemSelect();
+        }
+
+    }
+
+}
