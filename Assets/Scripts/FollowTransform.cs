9f3bff14775b67b9069d13621a73f98afe8cbccc
Eric Mouledoux <EricMouledoux@gmail.com>
2017-05-25 10:18:51 -0500

    follow script

---
+++ Assets/Scripts/FollowTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform m_target;
+	
+	void Update ()
+    {
+        transform.position = m_target.position;
+    }
+}
